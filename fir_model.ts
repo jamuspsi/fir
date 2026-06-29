5a84c6a859cf69ef6ac59aa014dd46af80b705a3
Brian Arsuaga <brian.arsuaga@gmail.com>
2026-06-29 02:13:36 -0500

    change to __contracts__ from keys and patchkeys

--- fir_model.ts
+++ fir_model.ts
@@ -6,6 +6,111 @@ const PatchKeysMap = new Map();
 export const ModelRegistry = new Map();
 const ChildClassMap = new Map();
 
+const MetaSymbol = Symbol('ModelMeta');
+
+export class ModelMeta {
+    model : Model;
+    constructor(model:Model) {
+        this.model = model;
+        this.children = [];
+        this.parent = null;
+        if(model !== Model) {
+            this.parent = Object.getPrototypeOf(model)
+            // console.log("Adding ", model.name, " to ", parent.name )
+            this.parent.__meta__.children.push(model);
+        }
+        if(this.parent) {
+            this.mro = [...this.parent.__meta__.mro, model]
+        } else {
+            this.mro = [model];
+        }
+
+        this.contracts = new Map();
+        this.parse_contracts();
+
+        // this.mro = this.calculate_mro();
+    }
+    parse_contracts() {
+        this.contract_recipes = new Map(); // name->['key', '$key', '-key', '*import']
+
+        // recipes are first copied from the parent
+        if(this.parent) {
+            for(var [name, fields] of this.parent.__meta__.contract_recipes) {
+                this.contract_recipes.set(name, [...fields]);
+            }
+        }
+        // then this model's contracts are appended to the end of each recipe
+        if(Object.hasOwn(this.model, '__contracts__')) {
+            for(var [name, fields] of Object.entries(this.model.__contracts__)) {
+                if(!this.contract_recipes.has(name)) {
+                    this.contract_recipes.set(name, []);
+                }
+                this.contract_recipes.get(name).push(...fields);
+            }
+        }
+        // next expandt the recipes' imports, careful of recursion.
+        var expanded_recipes = new Map();
+        const processing = [];
+        const expand_imports = (name)=>{
+            if(processing.includes(name)) {
+                throw new Error(`Circular contract definition while processing ${this.model.name}: ${processing}`);
+            }
+            processing.push(name);
+
+            var expanded = [];
+            for(var token of this.contract_recipes.get(name)) {
+                if(token.startsWith('*')) {
+                    // it's an import
+                    var import_name = token.substring(1);
+                    if(!expanded_recipes.has(import_name)) {
+                        if(!this.contract_recipes.get(import_name)) {
+                            throw new Error(`Unknown contract import on model ${this.model.name}, contract ${name} importing ${import_name}`);
+                        }
+                        expand_imports(import_name);
+                    }
+                    expanded.push(...expanded_recipes.get(import_name));
+                } else {
+                    expanded.push(token);
+                }
+            }
+            expanded_recipes.set(name, expanded);
+            processing.pop();
+        }
+
+        for( var [name, recipe] of this.contract_recipes ) {
+            expand_imports(name);
+        }
+
+        this.contracts = new Map(); // name->{transport:[], patch:[]}
+        const parse_recipe = (name, recipe)=>{
+            const contract = {
+                transport: new Set(),
+                patch: new Set(),
+            };
+            for(var token of recipe) {
+                if(token.startsWith('$')) {
+                    contract.transport.add(token.substring(1));
+                    contract.patch.add(token.substring(1));
+                } else if(token.startsWith('-')) {
+                    contract.transport.delete(token.substring(1));
+                    contract.patch.delete(token.substring(1));
+                } else {
+                    contract.transport.add(token);
+                }
+            }
+            contract.transport = [...contract.transport];
+            contract.patch = [...contract.patch];
+
+            this.contracts.set(name, contract);
+            // processing.pop();
+
+        }
+        for(var [name, recipe] of expanded_recipes) {
+            parse_recipe(name, recipe);
+        }
+
+    }
+}
 
 
 export class Model {
@@ -16,63 +121,74 @@ export class Model {
     // get obs() {
     //     return this[ObsStoreSymbol];
     // }
+    static get __meta__() {
+
+        if(!Object.hasOwn(this, MetaSymbol)) {
+            this[MetaSymbol] = new ModelMeta(this);
+        }
+        return this[MetaSymbol];
+    }
     static get __mro__() {
-        var mro = MroMap.get(this) || [];
-        if(mro.length) return mro;
+        return this.__meta__.mro;
+        // var mro = MroMap.get(this) || [];
+        // if(mro.length) return mro;
         
-        var kls = this;
-        var mro = [];
-        while(kls && kls != Function.prototype) {
-            mro.push(kls);
-            kls = Object.getPrototypeOf(kls);
-        }
-        MroMap.set(this, mro);
-        return mro;
+        // var kls = this;
+        // var mro = [];
+        // while(kls && kls != Function.prototype) {
+        //     mro.push(kls);
+        //     kls = Object.getPrototypeOf(kls);
+        // }
+        // MroMap.set(this, mro);
+        // return mro;
     }
-    static get __all_keys__() {
+    // static get __all_keys__() {
 
-        // this is the child prototype
-        var keys = KeysMap.get(this);
-        if(keys) return keys;
+    //     // this is the child prototype
+    //     var keys = KeysMap.get(this);
+    //     if(keys) return keys;
 
-        keys = [];
-        var kls = this;
-        const mro = this.__mro__;
+    //     keys = [];
+    //     var kls = this;
+    //     const mro = this.__mro__;
 
-        for(var x = mro.length - 1; x >= 0; x--) {
-            if(mro[x].hasOwnProperty('__keys__')) {
-                keys.push(...mro[x].__keys__);
-            }
-        }
-        KeysMap.set(this, keys);
-        return keys;
-    }
-    static get __all_patchkeys__() {
+    //     for(var x = mro.length - 1; x >= 0; x--) {
+    //         if(mro[x].hasOwnProperty('__keys__')) {
+    //             keys.push(...mro[x].__keys__);
+    //         }
+    //     }
+    //     KeysMap.set(this, keys);
+    //     return keys;
+    // }
+    // static get __all_patchkeys__() {
 
-        // this is the child prototype
-        var keys = PatchKeysMap.get(this);
-        if(keys) return keys;
+    //     // this is the child prototype
+    //     var keys = PatchKeysMap.get(this);
+    //     if(keys) return keys;
 
-        keys = [];
-        var kls = this;
-        const mro = this.__mro__;
+    //     keys = [];
+    //     var kls = this;
+    //     const mro = this.__mro__;
 
-        for(var x = mro.length - 1; x >= 0; x--) {
-            if(mro[x].hasOwnProperty('__patchkeys__')) {
-                keys.push(...mro[x].__patchkeys__);
-            }
-        }
-        PatchKeysMap.set(this, keys);
-        return keys;
-    }
+    //     for(var x = mro.length - 1; x >= 0; x--) {
+    //         if(mro[x].hasOwnProperty('__patchkeys__')) {
+    //             keys.push(...mro[x].__patchkeys__);
+    //         }
+    //     }
+    //     PatchKeysMap.set(this, keys);
+    //     return keys;
+    // }
     static get __child_classes__() {
-        var children = ChildClassMap.get(this);
-        if(children) return children;
-        children = [];
-        ChildClassMap.set(this, children);
-        return children;
+        return this.__meta__.children;
+        // var children = ChildClassMap.get(this);
+        // if(children) return children;
+        // children = [];
+        // ChildClassMap.set(this, children);
+        // return children;
     }
     static __register__(name) {
+        this.__meta__;
+
         name = name === undefined ? this.name : name;
         if(name) {
             if(ModelRegistry.has(name)) {
@@ -98,9 +214,9 @@ export class Model {
         return this === Model ? null : Object.getPrototypeOf(this);
     }
 
-    static to_plain_pojo(val) {
+    static to_plain_pojo(contract, val) {
         if(val instanceof Model) {
-            return val = val.as_jsonable();
+            return val = val.as_jsonable(contract);
         } else if(val && val.constructor === Date) {
             return {'__kls__': 'datetime', 'd': val.toISOString()};
         } else if(val && moment.isMoment(val)) {
@@ -108,13 +224,13 @@ export class Model {
             if(!date) return null;
             return {'__kls__': 'datetime', 'd': val.toDate().toISOString()}
         } else if(val && val instanceof Array) {
-            return val.map(Model.to_plain_pojo);                
+            return val.map(x=>Model.to_plain_pojo(contract, x));                
         } else if(val instanceof Promise) {
             return undefined;
         } else if(val && typeof(val) == 'object') {
             var cleaned = {};
             for(const [subkey, subvalue] of Object.entries(val)) {
-                cleaned[subkey] = Model.to_plain_pojo(subvalue);
+                cleaned[subkey] = Model.to_plain_pojo(contract, subvalue);
             }
             return cleaned;
         } else if(typeof(val) === 'number' && (val % 1 || val > 2**31-1 || val < -(2**31))) {
@@ -125,8 +241,9 @@ export class Model {
             return val;
         }
     }
-    as_jsonable() {
-        const keys = this.__class__.__all_keys__;
+    as_jsonable(contract) {
+        const keys = this.__class__.__meta__.contracts.get(contract).transport;
+        // const keys = this.__class__.__all_keys__;
         const jsonable = {
             "__kls__": this.__class__.name,
         };
@@ -134,14 +251,14 @@ export class Model {
         for(var k of keys) {
             // console.log(k);
             var val = this[k];
-            val = Model.to_plain_pojo(val);
+            val = Model.to_plain_pojo(contract, val);
             jsonable[k] = val;
         }
         return jsonable;
     }
-    static from_pojo(pojo) {
+    static from_pojo(contract, pojo) {
         if(pojo instanceof Array) {
-            return pojo.map(Model.from_pojo);
+            return pojo.map(sub_pojo=>Model.from_pojo(contract, sub_pojo));
         } else if(pojo && typeof pojo === 'object') {
             const kls = pojo.__kls__;
             switch(kls) {
@@ -149,7 +266,7 @@ export class Model {
                 case undefined:
                     var rehydrated = {}
                     for(const [subkey, subvalue] of Object.entries(pojo)) {
-                        rehydrated[subkey] = Model.from_pojo(subvalue);
+                        rehydrated[subkey] = Model.from_pojo(contract, subvalue);
                     }
                     return rehydrated;
                 case 'Decimal':
@@ -162,30 +279,31 @@ export class Model {
                         console.error('Model registry keys are ', [...ModelRegistry.keys()]);
                         throw `Could not find a model for ${kls}, make sure it was registered.`;
                     }
-                    return model.create_from_jsonable(pojo); 
+                    return model.create_from_jsonable(contract, pojo); 
             }
         } else {
             return pojo;
         }
     }
-    static create_from_jsonable(jsonable) {
+    static create_from_jsonable(contract, jsonable) {
         var inst = new this();
-        const keys = this.__all_keys__;
+        const keys = this.__meta__.contracts.get(contract).transport;
+
         for(const k of keys) {
             // try to assign them in key order, but skip any that
             // the json didn't specify.
-            if(jsonable.hasOwnProperty(k)) {
-                inst[k] = Model.from_pojo(jsonable[k])
+            if(Object.hasOwn(jsonable, k)) {
+                inst[k] = Model.from_pojo(contract, jsonable[k])
             }
         }
         return inst;
     }
-    dumps() {
-        return JSON.stringify(this.as_jsonable());
+    dumps(contract) {
+        return JSON.stringify(this.as_jsonable(contract));
     }
-    static loads(json) {
+    static loads(contract, json) {
         const jsonable = JSON.parse(json);
-        return Model.from_pojo(jsonable);
+        return Model.from_pojo(contract, jsonable);
     }
     clone() {
         return Model.loads(this.dumps());
@@ -193,5 +311,14 @@ export class Model {
 
     static __keys__ = ['pk'];
     static __patchkeys__ = ['pk'];
+
+    static __contracts__ = {
+        default: ['$pk'],
+    }
 }
 Model.__register__();
+
+
+/* Okay.
+
+*/
